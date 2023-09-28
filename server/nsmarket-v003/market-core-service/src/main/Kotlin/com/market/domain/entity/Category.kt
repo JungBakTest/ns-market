1cfe2ddd3692de5cd48dba0a12714c83f32562b1
Bak jun geon <pjg3239@naver.com>
2023-09-28 23:55:37 +0900

    refactor: 코드 수정 및 정리

--- server/nsmarket-v003/market-core-service/src/main/Kotlin/com/market/domain/entity/Category.kt
+++ server/nsmarket-v003/market-core-service/src/main/Kotlin/com/market/domain/entity/Category.kt
@@ -1,19 +1,19 @@
-package com.market.domain.entity
-
-import javax.persistence.*
-
-@Entity
-class Category (
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
-
-    val name: String = "",
-
-    @ManyToOne
-    val parentCategory: Category? = null,
-
-    @OneToMany(mappedBy = "parentCategory", cascade = [CascadeType.ALL])
-    val subCategories: MutableList<Category> = mutableListOf()
-
-)
+//package com.market.domain.entity
+//
+//import javax.persistence.*
+//
+//@Entity
+//class Category (
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    val id: Long? = null,
+//
+//    val name: String = "",
+//
+//    @ManyToOne
+//    val parentCategory: Category? = null,
+//
+//    @OneToMany(mappedBy = "parentCategory", cascade = [CascadeType.ALL])
+//    val subCategories: MutableList<Category> = mutableListOf()
+//
+//)
